d82c03cfdad720245c22c7b591e2cbdb22571a43
Ilia Burakov <burakov.ilia@gmail.com>
2025-12-24 01:08:46 -0500

    remove run.py

--- ytdl_inline_bot/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Entry point script for running the YouTube downloader inline bot."""
-
-import asyncio
-from .main import main
-
-if __name__ == '__main__':
-    asyncio.run(main())
